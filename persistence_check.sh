64a0c1639357c559433e9fd9ec6cdda98891094b
joelmclean <joel.mclean@micron21.com>
2026-05-01 15:02:18 +1000

    Create persistence_check.sh

---
+++ persistence_check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# Modified CRON
+crontab -l
+find /etc/cron* -mtime -4 -type f
+
+# Modified SSH Access
+find /home/*/.ssh/authorized_keys -type f -mtime -4
+find /home/*/.ssh/authorized_keys2 -type f -mtime -4
+find /home/*/.ss* -type f -mtime -4
+
+# Modified User Passwords (will show up if we've rolled the password
+find /etc/group -type f -mtime -4
+find /etc/shadow -type f -mtime -4
+
+# Modified user profile files
+find /home/*/.b* -type f -mtime -4
+
+# Review lastlog
+lastlog | grep 2026
+
+# Open Connections
+netstat -antp | grep ESTABLISHED | grep 443
+netstat -antpu | grep ESTABLISHED | grep 53
